cc6286228b5948cfe835dce3280314efb0a285d7
wanglong <wanglong.anqing@gmail.com>
2025-04-15 21:26:23 +0800

    cho: delete electron-log

--- main.js
+++ main.js
@@ -3,44 +3,6 @@ import { app, BrowserWindow, ipcMain } from "electron";
 import { getPort } from "get-port-please";
 import { startServer } from "next/dist/server/lib/start-server.js";
 import { join } from "path";
-import log from 'electron-log';
-
-log.transports.file.fileName = 'main.log';
-log.transports.file.format = '[{y}-{m}-{d} {h}:{i}:{s}.{ms}] [{level}]{scope} {text}';
-log.transports.file.level = false;  // Corrected from string 'false' to boolean false
-log.transports.console.level = false;  // Corrected from string 'false' to boolean false
-log.transports.file.maxSize = 1048576;
-const userLog = log.scope('user');
-userLog.info('用户范围的消息');
-app.on('ready', async () => {
-  // 渲染进程崩溃
-  app.on('renderer-process-crashed', (event, webContents, killed) => {
-    log.error(
-      `APP-ERROR:renderer-process-crashed; event: ${JSON.stringify(event)}; webContents:${JSON.stringify(
-        webContents
-      )}; killed:${JSON.stringify(killed)}`
-    );
-  });
-
-  // GPU进程崩溃
-  app.on('gpu-process-crashed', (event, killed) => {
-    log.error(`APP-ERROR:gpu-process-crashed; event: ${JSON.stringify(event)}; killed: ${JSON.stringify(killed)}`);
-  });
-
-  // 渲染进程结束
-  app.on('render-process-gone', async (event, webContents, details) => {
-    log.error(
-      `APP-ERROR:render-process-gone; event: ${JSON.stringify(event)}; webContents:${JSON.stringify(
-        webContents
-      )}; details:${JSON.stringify(details)}`
-    );
-  });
-
-  // 子进程结束
-  app.on('child-process-gone', async (event, details) => {
-    log.error(`APP-ERROR:child-process-gone; event: ${JSON.stringify(event)}; details:${JSON.stringify(details)}`);
-  });
-});
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
